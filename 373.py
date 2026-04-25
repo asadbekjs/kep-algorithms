6f7fe4ee9b17871bb09c71b8ae24140755f93156
asadbekjs <asadbek_rahimov@mail.ru>
2026-04-25 18:12:38 +0500

    Add algorithms for prime checking, maximum value, minimum position, index difference, and duplicate counting

---
+++ 373.py
@@ -0,0 +1,16 @@
+n = int(input())
+m = list(map(int,input().split()))
+# print(max(m))
+max_value = m[0]
+for element in m:
+    if element > max_value:
+        max_value = element
+
+print(max_value)
+
+# min_value = m[0]
+# for element in m:
+#     if element < min_value:
+#         min_value = element
+
+# print(min_value)
